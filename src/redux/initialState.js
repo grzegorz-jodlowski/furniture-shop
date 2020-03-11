3ee40a59ec6b99de130297e04caf4a9c0d7294f2
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-03-11 17:38:29 +0100

    Add styles to 'favorite' and 'compare' buttons
    Added styles due to boolean values in initian state of products (favorite, compare). Button click isn't handle yet.

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -15,6 +15,8 @@ const initialState = {
       stars: 2,
       promo: 'sale',
       newFurniture: true,
+      favorite: true,
+      compare: true,
     },
     {
       id: 'aenean-ru-bristique-2',
@@ -24,6 +26,8 @@ const initialState = {
       stars: 2,
       promo: 'sale',
       newFurniture: true,
+      favorite: true,
+      compare: false,
     },
     {
       id: 'aenean-ru-bristique-3',
@@ -33,6 +37,8 @@ const initialState = {
       stars: 2,
       promo: 'sale',
       newFurniture: true,
+      favorite: false,
+      compare: true,
     },
     {
       id: 'aenean-ru-bristique-4',
