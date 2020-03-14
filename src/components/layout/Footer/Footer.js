606509943b9a753712124f68e8716f8fc61faf68
daniel lukasik <daniel.lukasik10@gmail.com>
2020-03-14 12:47:34 +0100

    fixed footer RWD

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -17,7 +17,7 @@ const Footer = ({ children }) => (
     <div className={styles.footerMenu}>
       <div className='container'>
         <div className='row'>
-          <div className='col'>
+          <div className='col-xs-1 col-md-6 col-lg '>
             <div className={styles.menuWrapper}>
               <h6>Information</h6>
               <ul>
@@ -36,7 +36,7 @@ const Footer = ({ children }) => (
               </ul>
             </div>
           </div>
-          <div className='col'>
+          <div className='col-xs-1 col-md-6 col-lg '>
             <div className={styles.menuWrapper}>
               <h6>My account</h6>
               <ul>
@@ -55,7 +55,7 @@ const Footer = ({ children }) => (
               </ul>
             </div>
           </div>
-          <div className='col'>
+          <div className='col-xs-1 col-md-6 col-lg '>
             <div className={styles.menuWrapper}>
               <h6>Information</h6>
               <ul>
@@ -74,7 +74,7 @@ const Footer = ({ children }) => (
               </ul>
             </div>
           </div>
-          <div className='col'>
+          <div className='col-xs-1 col-md-6 col-lg '>
             <div className={styles.menuWrapper}>
               <h6>Orders</h6>
               <ul>
@@ -99,12 +99,14 @@ const Footer = ({ children }) => (
     </div>
     <div className={styles.bottomBar}>
       <div className='container'>
-        <div className='row align-items-center'>
-          <div className='col'></div>
-          <div className={'col text-center ' + styles.copyright}>
+        <div className='row align-items-center justify-content-between'>
+          <div className='col-sm-12 col-lg-3 text-center text-lg-left '></div>
+          <div
+            className={'col-sm-6 col-lg-3 text-left text-lg-center ' + styles.copyright}
+          >
             <p>©Copyright 2016 Bazar | All Rights Reserved</p>
           </div>
-          <div className={'col text-right ' + styles.socialMedia}>
+          <div className={'col-sm-6 col-lg-3 text-right ' + styles.socialMedia}>
             <ul>
               <li>
                 <a href='#'>
