d6bf04d654038538563758223fe2bf836b6d0117
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-03-17 18:35:31 +0100

    Add sample Bootstrap dropdown

--- src/components/layout/MenuBar/MenuBar.js
+++ src/components/layout/MenuBar/MenuBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+// import Dropdown from 'react-bootstrap/Dropdown';
+// import DropdownButton from 'react-bootstrap/DropdownButton';
 import ProductSearch from '../../features/ProductSearch/ProductSearch';
 
 import styles from './MenuBar.module.scss';
@@ -12,6 +13,11 @@ const MenuBar = ({ children }) => (
         <div className={'col ' + styles.search}>
           <ProductSearch />
           <div className={styles.hamburger}>&#9776;</div>
+          {/* <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </DropdownButton> */}
         </div>
         <div className={'col-auto ' + styles.menu}>
           <ul>
