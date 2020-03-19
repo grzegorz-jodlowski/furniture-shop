f3d5ba702d388e2fc14810be912bd40ec946640a
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-03-19 20:32:29 +0100

    create new component Gallery

---
+++ src/components/features/Gallery/Gallery.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Gallery.module.scss';
+
+const Gallery = () => {
+  return  <div className={styles.root}>fadfadfadf</div>;
+};
+
+export default Gallery;
