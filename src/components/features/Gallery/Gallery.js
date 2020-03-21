ab26a2839c40c322416f8d120a37c4f91fa0f8e7
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-03-21 17:53:57 +0100

    add bassic Gallery elements

--- src/components/features/Gallery/Gallery.js
+++ src/components/features/Gallery/Gallery.js
@@ -2,7 +2,39 @@ import React from 'react';
 import styles from './Gallery.module.scss';
 
 const Gallery = () => {
-  return  <div className={styles.root}>fadfadfadf</div>;
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div  className='row'>
+          <dic className='col-6'>
+            <div className={styles.heading}>
+              <h3>Furniture gallery</h3>
+            </div>
+            <div className={styles.menu}>
+              <span>category</span>
+              <ul>
+                <li>
+                  <span>Featured</span>
+                </li>
+                <li>
+                  <span>Top seller</span>
+                </li>
+                <li>
+                  <span>Sale off</span>
+                </li>
+                <li>
+                  <span>Top rated</span>
+                </li>
+              </ul>
+            </div>
+            <div className={styles.product}>product</div>
+            <div className={styles.slider}>slider</div>
+          </dic>
+          <div className='col-6'>picture</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Gallery;
