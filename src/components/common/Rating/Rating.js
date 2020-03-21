43de8d3e0f88481e64f9eaed2d9a2169e49456f4
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-03-21 15:27:03 +0100

    Add interactive hover for rating stars

--- src/components/common/Rating/Rating.js
+++ src/components/common/Rating/Rating.js
@@ -7,6 +7,10 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
 
 class Rating extends React.Component {
+  state = {
+    tempRate: null,
+  };
+
   static propTypes = {
     id: PropTypes.string,
     stars: PropTypes.number,
@@ -18,20 +22,44 @@ class Rating extends React.Component {
     this.props.updateUserRate(this.props.id, rating);
   }
 
+  starOver(rating) {
+    this.setState({
+      tempRate: rating,
+    });
+  }
+
+  starOut() {
+    this.setState({
+      tempRate: null,
+    });
+  }
+
   render() {
     const { stars, userRate } = this.props;
     return [1, 2, 3, 4, 5].map(i => (
       <a key={i}>
         {i <= (userRate || stars) ? (
           <FontAwesomeIcon
-            className={userRate ? styles.userRate : ''}
+            className={`${this.state.tempRate >= i ? styles.tempRate : ''} ${
+              userRate ? styles.userRate : ''
+            }`}
             onClick={this.rate.bind(this, i)}
             icon={faStar}
+            onMouseOver={this.starOver.bind(this, i)}
+            onMouseOut={this.starOut.bind(this)}
           >
             {i} stars
           </FontAwesomeIcon>
         ) : (
-          <FontAwesomeIcon onClick={this.rate.bind(this, i)} icon={farStar}>
+          <FontAwesomeIcon
+            className={`${this.state.tempRate >= i ? styles.tempRate : ''} ${
+              userRate ? styles.userRate : ''
+            }`}
+            onClick={this.rate.bind(this, i)}
+            icon={farStar}
+            onMouseOver={this.starOver.bind(this, i)}
+            onMouseOut={this.starOut.bind(this)}
+          >
             {i} stars
           </FontAwesomeIcon>
         )}
