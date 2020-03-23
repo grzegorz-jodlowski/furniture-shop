e15684044cda844aa292a8a38faa972a4cb89fc1
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-03-23 12:38:43 +0100

    Refactor MainLayout comp from functional to class comp

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -4,13 +4,17 @@ import PropTypes from 'prop-types';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
-const MainLayout = ({ children }) => (
-  <div>
-    <Header />
-    {children}
-    <Footer />
-  </div>
-);
+class MainLayout extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        {this.props.children}
+        <Footer />
+      </div>
+    );
+  }
+}
 
 MainLayout.propTypes = {
   children: PropTypes.node,
