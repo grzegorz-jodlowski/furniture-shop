a1bd21be31988a3fa4b316c4f7b2dab0734a0d02
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-03-26 18:45:45 +0100

    Add Brands component structure and logic

--- src/components/features/Brands/Brands.js
+++ src/components/features/Brands/Brands.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import styles from './Brands.module.scss';
 import PropTypes from 'prop-types';
+import Button from '../../common/Button/Button';
 
-const Brands = ({ brands }) => {
-  return <div className={styles.component}>{console.log(brands)}</div>;
-};
+const Brands = ({ brands }) => (
+  <div className={styles.root}>
+    <div className='container'>
+      <div className='row'>
+        <div className='col '>
+          <div className={styles.slider}>
+            <Button className={styles.preview}>
+              <p>{'<'}</p>
+            </Button>
+            <div className={styles.thumbnails}>
+              {brands.slice(0, 6).map(brand => (
+                <div key={brand.id} className={styles.thumbnail}>
+                  <img src={brand.photo} alt={brand.name} />
+                </div>
+              ))}
+            </div>
+            <Button className={styles.next}>
+              <p>{'>'}</p>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 Brands.propTypes = {
   brands: PropTypes.array,
