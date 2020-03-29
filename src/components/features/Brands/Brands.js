22c9371f0eead248d7a35402a280703d9d5b5a04
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-03-29 13:24:48 +0200

    Refactor preview, next button from html to css

--- src/components/features/Brands/Brands.js
+++ src/components/features/Brands/Brands.js
@@ -6,24 +6,16 @@ import Button from '../../common/Button/Button';
 const Brands = ({ brands }) => (
   <div className={styles.root}>
     <div className='container'>
-      <div className='row'>
-        <div className='col '>
-          <div className={styles.slider}>
-            <Button className={styles.preview}>
-              <p>{'<'}</p>
-            </Button>
-            <div className={styles.thumbnails}>
-              {brands.slice(0, 6).map(brand => (
-                <div key={brand.id} className={styles.thumbnail}>
-                  <img src={brand.photo} alt={brand.name} />
-                </div>
-              ))}
+      <div className={styles.slider}>
+        <Button className={styles.preview}></Button>
+        <div className={styles.thumbnails}>
+          {brands.slice(0, 6).map(brand => (
+            <div key={brand.id} className={styles.thumbnail}>
+              <img src={brand.photo} alt={brand.name} />
             </div>
-            <Button className={styles.next}>
-              <p>{'>'}</p>
-            </Button>
-          </div>
+          ))}
         </div>
+        <Button className={styles.next}></Button>
       </div>
     </div>
   </div>
