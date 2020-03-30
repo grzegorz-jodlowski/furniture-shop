d86995cac19a01762c6616a00682e27c3603fb06
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-03-30 13:22:20 +0200

    add RWD to search

--- src/components/features/ProductSearch/ProductSearch.js
+++ src/components/features/ProductSearch/ProductSearch.js
@@ -6,31 +6,55 @@ import { faListUl, faSearch, faCaretDown } from '@fortawesome/free-solid-svg-ico
 
 import styles from './ProductSearch.module.scss';
 
-const ProductSearch = () => (
-  <form action='' className={styles.root}>
-    <div className={styles.category}>
-      <FontAwesomeIcon className={styles.icon} icon={faListUl} />
-      <ul className={styles.list}>
-        <li className={styles.listCategory}>
-          <span value=''>Select a category</span>
-        </li>
-        <li className={styles.activeList}>
-          <span value=''>category one</span>
-        </li>
-        <li className={styles.activeList}>
-          <span value=''>category two</span>
-        </li>
-      </ul>
-      <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
+const ProductSearch = () => {
+
+  const mql = window.matchMedia('(max-width:  768px)');
+  let mobile = false;
+  mediaqueryresponse(mql);
+  mql.addListener(mediaqueryresponse);
+  function mediaqueryresponse(mql) {
+    if (mql.matches) {
+      mobile = true;
+    }
+  }
+
+  return (
+    <div className={mobile ? styles.wrapMd : ''}>
+      <form action='' className={styles.root + ' ' + (mobile ? styles.mobile : '')}>
+        <div className={styles.category}>
+          <FontAwesomeIcon className={styles.icon} icon={faListUl} />
+          <ul className={styles.list}>
+            <li className={styles.listCategory}>
+              <span value=''>Select a category</span>
+            </li>
+            <li className={styles.activeList}>
+              <span value=''>Bedroom</span>
+            </li>
+            <li className={styles.activeList}>
+              <span value=''>Kitchet</span>
+            </li>
+            <li className={styles.activeList}>
+              <span value=''>Livingroom</span>
+            </li>
+            <li className={styles.activeList}>
+              <span value=''>Bathroom</span>
+            </li>
+            <li className={styles.activeList}>
+              <span value=''>Kids</span>
+            </li>
+          </ul>
+          <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
+        </div>
+        <div className={styles.searchField}>
+          <input placeholder='Search products...' type='text' />
+          <button>
+            <FontAwesomeIcon className={styles.icon} icon={faSearch} />
+          </button>
+        </div>
+      </form>
     </div>
-    <div className={styles.searchField}>
-      <input placeholder='Search products...' type='text' />
-      <button>
-        <FontAwesomeIcon className={styles.icon} icon={faSearch} />
-      </button>
-    </div>
-  </form>
-);
+  );
+};
 
 ProductSearch.propTypes = {
   children: PropTypes.node,
